c78b85b2cef220d2651cc46b99902442efaada92
hy2632 <hy2632@columbia.edu>
2019-12-09 00:28:09 +0000

    12.8 hy Create Model

--- squirrel/models.py
+++ squirrel/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Squirrel(models.Model):
+	Latitude = models.FloatField()
+	Longitude = models.FloatField()
+	Unique_Squirrel_ID = models.CharField(max_length=30,primary_key=True)
+	Shift_Choices = (
+		('AM','AM'),
+		('PM','PM'),
+	)
+	Shift = models.CharField(max_length=2, choices=Shift_Choices)
+	Date = models.IntegerField()
+
+	Age_Choices = (
+		('', '----'),
+		('Adult','Adult'),
+		('Juvenile','Juvenile'),
+		('?','Unknown')
+	)
+	Age = models.CharField(max_length=10, choices=Age_Choices, blank=True)
+
+
+	Primary_Fur_Color = models.CharField(max_length=30, blank=True)
+	Location = models.CharField(max_length=30, blank=True)
+	Specific_Location = models.CharField(max_length=100, blank=True)
+
+	TF_Choices = (
+		# ('', '----'),
+		('true','True'),
+		('false','False'),
+	)
+	Running = models.CharField(max_length=10,choices=TF_Choices)
+	Chasing = models.CharField(max_length=10,choices=TF_Choices)
+	Climbing = models.CharField(max_length=10,choices=TF_Choices)
+	Eating = models.CharField(max_length=10,choices=TF_Choices)
+	Foraging = models.CharField(max_length=10,choices=TF_Choices)
+
+
+	Other_Activities = models.CharField(max_length=100, blank=True)
+	
+	Kuks = models.CharField(max_length=10,choices=TF_Choices)
+	Quaas = models.CharField(max_length=10,choices=TF_Choices)
+	Moans = models.CharField(max_length=10,choices=TF_Choices)
+	Tail_flags = models.CharField(max_length=10,choices=TF_Choices)
+	Tail_twitches = models.CharField(max_length=10,choices=TF_Choices)
+	Approaches = models.CharField(max_length=10,choices=TF_Choices)
+	Indifferent = models.CharField(max_length=10,choices=TF_Choices)
+	Runs_from = models.CharField(max_length=10,choices=TF_Choices)
+	
+	def __str__(self):
+		return self.Unique_Squirrel_ID
--- squirrel/views.py
+++ squirrel/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
+
+from .models import Squirrel
+from .forms import SquirrelForm #Build a Form
+
+def index(request):
+	sightings = Squirrel.objects.all()
+	context={'sightings':sightings}
+	return render(request, 'sightings/index.html',context)
+	
+def update(request,sqr_id):
+	sqr = Squirrel.objects.get(Unique_Squirrel_ID=sqr_id)
+	if request.method =='POST':
+		form = SquirrelForm(request.POST, instance=sqr)
+		if form.is_valid():
+			form.save()
+			return redirect(f'/sightings/')
+	else:
+		form = SquirrelForm(instance=sqr)
+
+	context={
+		'form':form,
+	}
+	return render(request, 'sightings/update.html',context)
+
+def add(request):
+	if request.method == 'POST':
+		form = SquirrelForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect(f'/sightings/')
+	else:
+		form = SquirrelForm
+
+	context={
+		'form':form,
+	}
+	return render(request, 'sightings/add.html', context)
+
+def map(request):
+	sightings = Squirrel.objects.order_by('?')[0:99]
+	context={'sightings':sightings}
+	return render(request, 'map/map.html',context)
+
+
+
