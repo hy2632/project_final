dbcafe3cc2adb29c1748816c4439a514286195d1
hy2632 <hy2632@columbia.edu>
2019-12-09 06:38:30 +0000

    Add stats.

--- sightings/urls.py
+++ sightings/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'sightings'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('stats',views.stats),
     path('add',views.add),
     path('<str:sqr_id>',views.update),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -6,7 +6,7 @@ from .models import Squirrel
 from .forms import SquirrelForm #Build a Form
 
 def index(request):
-	sightings = Squirrel.objects.all()
+	sightings = Squirrel.objects.all().order_by('Unique_Squirrel_ID')
 	context={'sightings':sightings}
 	return render(request, 'sightings/index.html',context)
 	
@@ -39,4 +39,41 @@ def add(request):
 	}
 	return render(request, 'sightings/add.html', context)
 
+def stats(request):
+	squirrel_count = Squirrel.objects.all().count()
 
+	Running_True=0
+	for i in Squirrel.objects.values_list('Running'):
+		if 'true' in i:
+			Running_True+=1
+
+	Chasing_True=0
+	for i in Squirrel.objects.values_list('Chasing'):
+		if 'true' in i:
+			Chasing_True+=1
+
+	Climbing_True=0
+	for i in Squirrel.objects.values_list('Climbing'):
+		if 'true' in i:
+			Climbing_True+=1
+
+	Eating_True=0
+	for i in Squirrel.objects.values_list('Eating'):
+		if 'true' in i:
+			Eating_True+=1
+
+	Foraging_True=0
+	for i in Squirrel.objects.values_list('Foraging'):
+		if 'true' in i:
+			Foraging_True+=1
+
+
+	context={
+			'squirrel_count':squirrel_count,
+			'Running_True':Running_True,
+			'Chasing_True':Chasing_True,
+			'Climbing_True':Climbing_True,
+			'Eating_True':Eating_True,
+			'Foraging_True':Foraging_True,
+			}
+	return render(request, 'sightings/stats.html',context)
